50872e93d0ed8ddfb43c7d0b68973515ad1ab998
elnur004 <lnur004@gmail.com>
2020-11-21 21:27:28 +0400

    added rollingn the dice and switching the active player

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // Selecting elements
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
@@ -16,8 +18,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRollEl.addEventListener('click', function() {
@@ -33,9 +36,14 @@ btnRollEl.addEventListener('click', function() {
     if(dice > 1) {
         // Add dice to the current score
         currentScore += dice;
-        current0El.textContent = currentScore;  // CHANGE PLAYER LATER
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         // Switch to next player
-
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;  
+        player0.classList.toggle('player--active');
+        player1.classList.toggle('player--active');
+        diceEl.classList.add('hidden');
     }
 });
